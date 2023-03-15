0dbd591a26c0e0f81c0be8bb76ab67cc689caf5d
Caro Trogliero <carolinatrogliero@gmail.com>
2023-03-14 22:07:28 -0300

    actulizacion de bbdd escuelas

--- data/Actualizacion_Data_Escuelas.py
+++ data/Actualizacion_Data_Escuelas.py
@@ -12,15 +12,15 @@ VAR_COD_POSTAL = 'código postal'
 
 
 #ARCHIVO CON INFORMACIÓN PROVINCIAL > DE ACA SE TOMA EL LISTADO DE VALIDACIÓN
-establec_educativos_completo=gpd.read_file("./data/establecimientos_educativos_provincial.geojson")
+establec_educativos_completo=gpd.read_file("./data/escuelas_en_parcelas.geojson")
 
 #Limpieza y ordenamiento de los datos
-establec_educativos_completo['direccion_gj'] = establec_educativos_completo['dirección calle'] + establec_educativos_completo['dirección nro.']
-establec_educativos_completo['característica telefónica'] = '(' + establec_educativos_completo['característica telefónica'].fillna(0).astype(int).astype(str) + ')'
-establec_educativos_completo['telefono_gj'] = establec_educativos_completo['característica telefónica'] +' ' + establec_educativos_completo['teléfono'].astype(str)
+establec_educativos_completo['direccion_gj'] = establec_educativos_completo['dirección.calle'] + establec_educativos_completo['dirección.nro.']
+establec_educativos_completo['característica.telefónica'] = '(' + establec_educativos_completo['característica.telefónica'].fillna(0).astype(int).astype(str) + ')'
+establec_educativos_completo['telefono_gj'] = establec_educativos_completo['característica.telefónica'] +' ' + establec_educativos_completo['teléfono'].astype(str)
 establec_educativos_completo = establec_educativos_completo.rename(columns = {'email': 'mail_gj'})
 
-establec_educativos_completo[VAR_CODIGO_UNICO] = establec_educativos_completo[VAR_COD_POSTAL].fillna(0).astype(int).astype(str)+ ' - ' +  establec_educativos_completo[VAR_CUE].astype(str) + ' - ' + establec_educativos_completo[VAR_SEDE_ANEXO_EXT]
+establec_educativos_completo[VAR_CODIGO_UNICO] = establec_educativos_completo['código.postal'].fillna(0).astype(int).astype(str)+ ' - ' +  establec_educativos_completo[VAR_CUE].astype(str) + ' - ' + establec_educativos_completo['sede.axo.ext']
 
 #listado_validacion_escuelas = establec_educativos_completo[['cue', 'nombre establecimiento', 'partido']].sort_values(by='cue')
 #listado_validacion_escuelas.to_csv('listado_validacion_est_educ_provincia.csv', sep=';')
@@ -47,19 +47,20 @@ base_escuelas_actualizada['email'] = base_escuelas_actualizada['mail_manual'].fi
 base_escuelas_actualizada['Tel'] = base_escuelas_actualizada['telefono_manual'].fillna(base_escuelas_actualizada['telefono_gj'])
 base_escuelas_actualizada['direccion'] = base_escuelas_actualizada['direccion_manual'].fillna(base_escuelas_actualizada['direccion_gj']).str.title()
 
+base_escuelas_actualizada_gsjon = gpd.GeoDataFrame(base_escuelas_actualizada, geometry = 'geometry', crs='EPSG:4326')  #epsg4326 is WGS84
 
+base_escuelas_actualizada_gsjon.to_file('./data/escuelas_informacion_actualizadas.geojson', driver='GeoJSON')
 
-#actualizado_check =gpd.read_file("./data/escuelas_informacion_actualizadas.geojson")
+# actualizado_check =gpd.read_file("./data/escuelas_informacion_actualizadas.geojson")
 
-#print(actualizado_check)
+# print(actualizado_check)
 
-#base_escuelas_actualizada_gsjon = gpd.GeoDataFrame(base_escuelas_actualizada, geometry = 'geometry', crs='EPSG:4326')  #epsg4326 is WGS84
-
-#base_escuelas_actualizada_gsjon.to_file('escuelas_informacion_actualizadas.geojson', driver='GeoJSON')
-
-
-#establec_educativos_PARQUET=gpd.read_parquet("./data/escuelas_parcelas.parquet")
 
 ##FALTA AGREGAR CASOS NUEVOS
+
+
+
+
+
 
 
