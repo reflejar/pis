4be9266b9041f85ddef55f61d9690d4612c64cc0
Pano Valdez <marianovaldez92@protonmail.com>
2023-03-28 16:11:26 -0300

    deploy

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from app import server, app
 
 # Se corre la aplicación
 if __name__ == "__main__":
-	server.run(host="0.0.0.0", port=8050, debug=True, use_reloader=True)
+	app.run(host="0.0.0.0", port=8050, debug=True, use_reloader=True)
