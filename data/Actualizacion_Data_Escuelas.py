0da767fb14299ecd1908cdaed45f29eb05dae4d1
Caro Trogliero <carolinatrogliero@gmail.com>
2023-03-29 13:39:18 -0300

    actualizacion de datos de escuelas

--- data/Actualizacion_Data_Escuelas.py
+++ data/Actualizacion_Data_Escuelas.py
@@ -21,24 +21,28 @@ VAR_GEOMETRIA = 'geometry'
 establec_educativos_completo=gpd.read_file("./data/establecimientos-educativos-PBA.geojson")
 
 # LIMPIEZA Y ORDENAMIENTO DE DATOS
-establec_educativos_completo['direccion_gj'] = establec_educativos_completo['dirección calle'] + establec_educativos_completo['dirección nro.']
+establec_educativos_completo['direccion_indec'] = establec_educativos_completo['dirección calle'] + establec_educativos_completo['dirección nro.']
 establec_educativos_completo['característica telefónica'] = '(' + establec_educativos_completo['característica telefónica'].fillna(0).astype(int).astype(str) + ')'
-establec_educativos_completo['telefono_gj'] = establec_educativos_completo['característica telefónica'] +' ' + establec_educativos_completo['teléfono'].astype(str)
-establec_educativos_completo = establec_educativos_completo.rename(columns = {VAR_MAIL: 'mail_gj', VAR_LATITUD : 'latitud_gj', VAR_LONGITUD : 'longitud_gj'})
+establec_educativos_completo['telefono_indec'] = establec_educativos_completo['característica telefónica'] +' ' + establec_educativos_completo['teléfono'].astype(str)
+establec_educativos_completo = establec_educativos_completo.rename(columns = {VAR_MAIL: 'mail_indec', VAR_LATITUD : 'latitud_indec', VAR_LONGITUD : 'longitud_indec', VAR_GEOMETRIA : 'geometry_indec'})
 
 establec_educativos_completo[VAR_CODIGO_UNICO] = establec_educativos_completo[VAR_COD_POSTAL].fillna(0).astype(int).astype(str)+ ' - ' +  establec_educativos_completo[VAR_CUE].astype(str) + ' - ' + establec_educativos_completo[VAR_SEDE_ANEXO_EXT]
 
 # Base de datos actualizada manualmente
-escuelas_informacion_manual = pd.read_csv('data\est_educativos_actualizacion_provincial.csv', encoding = 'latin1', on_bad_lines='skip', sep = ";")
+escuelas_informacion_manual = pd.read_csv('data\est_educativos_actualizacion_provincial.csv', encoding = 'latin1', on_bad_lines='skip', sep = ";", decimal=",")
 escuelas_informacion_manual = escuelas_informacion_manual.dropna(how= 'all', axis=0)
 escuelas_informacion_manual = escuelas_informacion_manual.dropna(how= 'all', axis=1)
 
 # Filtramos para solo quedarnos con las escuelas rurales. Las urbanas se incluyen dentro de las localidades.
 escuelas_informacion_manual = escuelas_informacion_manual[escuelas_informacion_manual['ubicacion_manual']!= 'Urbano']
+
 # Construimos codigo para el merge
 escuelas_informacion_manual[VAR_CODIGO_UNICO] = escuelas_informacion_manual[VAR_COD_POSTAL].astype(int).astype(str) + ' - ' + escuelas_informacion_manual[VAR_CUE].astype(str) + ' - ' + escuelas_informacion_manual[VAR_SEDE_ANEXO_EXT]
+
 # Construimos nombre a mostrar del establecimiento
 escuelas_informacion_manual['nombre_apellido_manual'] = escuelas_informacion_manual['nombre'] + ' ' + escuelas_informacion_manual['apellido']
+escuelas_informacion_manual['longitud_manual'] = escuelas_informacion_manual['longitud_manual'].astype(float)
+escuelas_informacion_manual['latitud_manual'] = escuelas_informacion_manual['latitud_manual'].astype(float)
 
 ## ACHICAMOS LA BBDD A UNIR CON EL GEOJSON
 VARIABLES_A_ACTUALIZAR = ['codigo_unico_pis', 'nombre establecimiento a considerar', 'direccion_manual', 'cargo', 'nombre_apellido_manual', 'telefono_manual', 'mail_manual', 'matricula_2021', 'latitud_manual', 'longitud_manual', 'nueva_existente']
@@ -52,23 +56,22 @@ escuelas_nuevas_informacion_manual = escuelas_informacion_manual[escuelas_inform
 
 #### Unimos GeoJson con la info de escuelas existentes
 base_escuelas_actualizada = pd.merge(establec_educativos_completo , escuelas_existentes_informacion_manual, on = VAR_CODIGO_UNICO, how = 'left')
-
+base_escuelas_actualizada = base_escuelas_actualizada.drop(columns = ['geometry_indec'])
 
 if len(escuelas_nuevas_informacion_manual):
     base_escuelas_actualizada = pd.concat([base_escuelas_actualizada, escuelas_nuevas_informacion_manual])
 
+
 base_escuelas_actualizada[VAR_NOM_ESTABLEC] = base_escuelas_actualizada['nombre establecimiento a considerar'].fillna(base_escuelas_actualizada['nombre establecimiento']).str.title()
-base_escuelas_actualizada[VAR_MAIL] = base_escuelas_actualizada['mail_manual'].fillna(base_escuelas_actualizada['mail_gj']).str.lower()
-base_escuelas_actualizada[VAR_TELEFONO] = base_escuelas_actualizada['telefono_manual'].fillna(base_escuelas_actualizada['telefono_gj'])
-base_escuelas_actualizada[VAR_DIRECCION] = base_escuelas_actualizada['direccion_manual'].fillna(base_escuelas_actualizada['direccion_gj']).str.title()
-base_escuelas_actualizada[VAR_LATITUD] = base_escuelas_actualizada['latitud_manual'].fillna(base_escuelas_actualizada['latitud_gj'])
-base_escuelas_actualizada[VAR_LONGITUD] = base_escuelas_actualizada['longitud_manual'].fillna(base_escuelas_actualizada['longitud_gj'])
-#base_escuelas_actualizada[VAR_GEOMETRIA] = base_escuelas_actualizada['geometry_gj']
-#base_escuelas_actualizada['PRUEBA'] = { "type": "Point", "coordinates": 'PROBANDO' }
+base_escuelas_actualizada[VAR_MAIL] = base_escuelas_actualizada['mail_manual'].fillna(base_escuelas_actualizada['mail_indec']).str.lower()
+base_escuelas_actualizada[VAR_TELEFONO] = base_escuelas_actualizada['telefono_manual'].fillna(base_escuelas_actualizada['telefono_indec'])
+base_escuelas_actualizada[VAR_DIRECCION] = base_escuelas_actualizada['direccion_manual'].fillna(base_escuelas_actualizada['direccion_indec']).str.title()
+base_escuelas_actualizada[VAR_LATITUD] = base_escuelas_actualizada['latitud_manual'].fillna(base_escuelas_actualizada['latitud_indec'])
+base_escuelas_actualizada[VAR_LONGITUD] = base_escuelas_actualizada['longitud_manual'].fillna(base_escuelas_actualizada['longitud_indec'])
 
-base_escuelas_actualizada_gsjon = gpd.GeoDataFrame(base_escuelas_actualizada, geometry = 'geometry', crs='EPSG:4326')  #epsg4326 is WGS84
+base_escuelas_actualizada_gsjon = gpd.GeoDataFrame(base_escuelas_actualizada, geometry=gpd.points_from_xy(base_escuelas_actualizada[VAR_LONGITUD], base_escuelas_actualizada[VAR_LATITUD]), crs='EPSG:4326')
 
-base_escuelas_actualizada_gsjon.to_file('./data/escuelas_informacion_actualizada.geojson', driver='GeoJSON')
+base_escuelas_actualizada_gsjon.to_file('./data/escuelas_informacion_actualizada_prueba.geojson', driver='GeoJSON')
 
 #actualizado_check =gpd.read_file("./data/escuelas_informacion_actualizada.geojson")
 
