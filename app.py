0579a8b85883fe5c4601f20b38627d04036fe616
Pano Valdez <marianovaldez92@protonmail.com>
2023-04-03 15:57:55 -0300

    oculto vistas sci

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ from dash import html
 from tools.layout import Navbar, Footer
 
 from pages import (
-	index, mapa_normativo, 
-	gis_resultados, ranking,
-	digesto
+	index, 
+    # mapa_normativo, 
+	# gis_resultados, 
+    # ranking,
+	# digesto
 )
 
 
@@ -34,16 +36,16 @@ app = dash.Dash(
 	title="PIS | Pesticidas Introducidos Silenciosamente"
 )
 dash.register_page(index.__name__, title="Pesticidas Introducidos Silenciosamente", path='/', layout=index.layout)
-dash.register_page(mapa_normativo.__name__, title="Mapa Normativo", path='/mapa-normativo', layout=mapa_normativo.layout)
-dash.register_page(gis_resultados.__name__, title="Mapa Normativo", path='/mapa-resultados', layout=gis_resultados.layout)	
-dash.register_page(ranking.__name__, title="Mapa Normativo", path='/ranking', layout=ranking.layout)
-dash.register_page(digesto.__name__, title="Mapa Normativo", path='/digesto', layout=digesto.layout)
+# dash.register_page(mapa_normativo.__name__, title="Mapa Normativo", path='/mapa-normativo', layout=mapa_normativo.layout)
+# dash.register_page(gis_resultados.__name__, title="Mapa Normativo", path='/mapa-resultados', layout=gis_resultados.layout)	
+# dash.register_page(ranking.__name__, title="Mapa Normativo", path='/ranking', layout=ranking.layout)
+# dash.register_page(digesto.__name__, title="Mapa Normativo", path='/digesto', layout=digesto.layout)
 
 
 # Se agregan los componentes de la web
 app.layout = html.Div(
 	children=[
-		Navbar, 
+		Navbar,     
 		dash.page_container,
 		Footer
 	]
