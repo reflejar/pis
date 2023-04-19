3e35cff8e78153b74e7290c327f8354e8e9abad3
Pano Valdez <marianovaldez92@protonmail.com>
2023-04-19 17:32:30 -0300

    aire en proximamente version lg

--- pages/index/componentes/proximamente.py
+++ pages/index/componentes/proximamente.py
@@ -11,13 +11,13 @@ Proximamente = html.Section([
                         html.P("Conocé los resultados de la campaña de testeos en orina y análisis en el ambiente para saber dónde y en qué medida se detectaron trazas de agroquímicos.", className="text-white px-5")
                         
                     ], 
-                    md={'offset': 3, 'size': 3}),
+                    lg={'offset': 3, 'size': 3}),
                     dbc.Col([
                         html.Img(src="/assets/img/recursos/ranking.svg", height="120px"),
                         html.H4("Ranking Normativo", className="text-white mt-3 fw-bold"),
                         html.P("Conocé en qué puesto del ranking normativo está la regulación de agroquímicos de tu territorio para saber cómo puede mejorar.", className="text-white px-5")
                     ], 
-                    md=3),
+                    lg=3),
                 ], class_name="px-5"),
                 dbc.Row([
                     dbc.Col([
@@ -26,13 +26,13 @@ Proximamente = html.Section([
                         html.P("Descubrí el compendio de todos los fallos judiciales sobre el tema para saber en cuáles te podés apoyar y así exigir cambios.", className="text-white px-5")
                         
                     ], 
-                    md={'offset': 3, 'size': 3}),
+                    lg={'offset': 3, 'size': 3}),
                     dbc.Col([
                         html.Img(src="/assets/img/recursos/normativo.svg", height="120px"),
                         html.H4("PIS desde arriba", className="text-white mt-3 fw-bold"),
                         html.P("Desde una georreferenciación, conocé las restricciones municipales a las aplicaciones de agroquímicos para saber cuál está vigente en el lugar en que vivís.", className="text-white px-5")
                     ], 
-                    md=3),
+                    lg=3),
                 ], class_name="px-5"),                
                 dbc.Row(dbc.Col(html.H5("Suscribite a nuestro newsletter y seguí todos los lanzamientos.", className="mt-5 text-white "))),
                 dbc.Row(dbc.Col(dbc.Button("Noticias sin spamear", color="primary", className="my-5 text-black p-3 text-uppercase ", href="https://preguntarparaacordar.typeform.com/to/B8k4SNx5", target="_blank"))),
